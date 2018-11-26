ad8e79fd96eb35675a81c5a374aa9bb291e2114b
Amanda Mok <a.mok@berkeley.edu>
2018-11-26 14:33:53 -0800

    wflow_publish("./code/mainScript_revised.R")

---
+++ code/mainScript_revised.R
@@ -0,0 +1,44 @@
+rm(list=ls())
+
+# setwd("") ## set working directory to git repo
+
+# check installed packages
+libs <- c("idr",
+          "parallel",
+          "snow")
+notInstalled <- libs[!libs %in% rownames(installed.packages())]
+if(length(notInstalled)>0) {install.packages(notInstalled)}
+require(snow)
+
+nSim <- 100 # number of simulations to perform
+nPerm <- 1000 # number of permutations to perform
+deg <- 49 # degrees of freedom to simulate from
+nCores <- parallel::detectCores() # number of clusters to use for makeCluster()
+
+## source script files
+scriptFiles <- grep(".R", list.files(path="./code"), value=T)
+scriptFiles <- grep("main", scriptFiles, invert=T, value=T)
+sapply(scriptFiles, function(x) source(paste0("./code/",x)))
+
+## load simulated data
+params <- expand.grid(p1=c(0.05,0.20),
+                      p2=0.05,
+                      p3=c(0,0.25,0.5,0.75,1))
+params_names <- apply(params, 1, function(x) paste("p1",x[1],"p2",x[2],"p3",x[3], sep="_"))
+load(file="./data/simulations.Rda")
+
+## assess significance
+simFuns <- c("gsea", "IDR.func", "spearman", "trunSpearman")
+simFunsDir <- c(F, T, F, F)
+cl <- makeCluster(getOption("cl.cores", nCores))
+clusterExport(cl, list=ls())
+for(i in 1:length(params_names)) {
+  print(paste("assessing for", params_names[i]))
+  system.time({assign(params_names[i],
+                      value=parLapply(cl, dat[[i]],
+                                      function(paramSim) {
+                                        assessSig(paramSim, simFuns=simFuns, simFunsDir=simFunsDir, nPerm=nPerm)
+                                      }))})
+  save(list=c(params_names[i], file=paste0("./data/", params_names[1])))
+}
+stopCluster(cl)
