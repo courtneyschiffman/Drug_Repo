ab51f792cbf902bf1c5a3f1a3e68bbad0f23b8da
Amanda Mok <a.mok@berkeley.edu>
2018-12-04 15:08:43 -0800

    update plotResults

--- code/plotResults.R
+++ code/plotResults.R
@@ -1,9 +1,5 @@
 rm(list=ls())
 
-# load results files
-resultsFiles <- grep("noidr", list.files("./data"), value=T)
-for(x in resultsFiles) { load(paste0("./data/", x)) }
-
 plotResults <- function(permResults) {
   require(ggplot2)
   require(reshape)
@@ -39,18 +35,14 @@ plotResults <- function(permResults) {
   }
   # plot results
   params <- unlist(strsplit(deparse(substitute(permResults)), split="_"))
-  # plotTitle <- paste(sapply(1:(length(params)/2),
-  #                     function(x) {
-  #                       paste(params[2*(x-1)+1], params[2*x], sep="=")
-  #                     }),
-  #                    collapse=", ")
   plotTitle <- paste(100*as.numeric(params[6]), "% same direction,",
                      100*as.numeric(params[8]), "% diff. direction")
   histPlot <- ggplot(subset(allDat, allDat$stat==x), aes(value)) +
     geom_histogram(data=subset(allDat, allDat$type=="permutation"),
                    binwidth=0.05, fill="blue", alpha=0.5) +
     geom_dotplot(data=subset(allDat, allDat$type=="simulation"),
-                 binwidth=0.05, fill="red", alpha=0.5) +
+                 binwidth=0.05, fill="red", alpha=0.5,
+                 dotsize=ifelse(nStats==3, 1, 2)) +
     ggtitle(plotTitle) + xlab("") +
     facet_grid(.~stat) + theme_bw()
   if(nStats == 3) {
@@ -66,6 +58,10 @@ plotResults <- function(permResults) {
   grid.arrange(histPlot, pvaluePlot, nrow=2)
 }
 
+# load results files
+resultsFiles <- grep("noidr", list.files("./data"), value=T)
+for(x in resultsFiles) { load(paste0("./data/", x)) }
+
 plotResults(p1_0.05_p2_0.05_p3_0_p4_0)
 plotResults(p1_0.05_p2_0.05_p3_0_p4_0.5)
 plotResults(p1_0.05_p2_0.05_p3_0.5_p4_0)
@@ -76,6 +72,7 @@ plotResults(p1_0.05_p2_0.05_p3_1_p4_0)
 ## new results w/ idr
 newResults <- grep("noidr", grep("p4", list.files("./data"), value=T), invert=T, value=T)
 for(x in newResults) { load(paste0("./data/", x)) }
+
 plotResults(p1_0.05_p2_0.05_p3_0_p4_0)
 plotResults(p1_0.05_p2_0.05_p3_0.5_p4_0)
 plotResults(p1_0.05_p2_0.05_p3_1_p4_0)
